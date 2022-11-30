dc1a9237859ec20c02c0d4ab533c63b2e4a0de89
winnie54 <winnieeluu54@gmail.com>
2022-11-30 04:27:11 -0800

    prints alphabets in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for l in reversed(range(ord('a'), ord('z') + 1)):
-    print("{}".format(chr(l - 32) if l % 2 != 0 else chr(l)), end="")
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
